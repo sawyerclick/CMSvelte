4fb7c5798889ae9d4998b9d1de5eea08cf0be357
Sawyer Click <src2188@columbia.edu>
2022-02-27 16:11:12 -0600

    add vite-imagetools example + dev <head> files

--- src/main.js
+++ src/main.js
@@ -1,24 +1,32 @@
 import './app.postcss';
 
-import Fieri from '$lib/assets/fieri.jpeg?w=650&webp';
-import FieriSrcset from '$lib/assets/fieri.jpeg?w=300;600;900&webp&srcset';
+// https://www.npmjs.com/package/svelte-lazy-loader
+import { Image } from 'svelte-lazy-loader';
+
+// create images on-the-fly with vite-imagetools
+// https://www.npmjs.com/package/vite-imagetools
+import ImagePlaceholder from '$lib/assets/san-felipe-del-morro-castle.jpg?w=100&png&blur=5';
+import ImageSrc from '$lib/assets/san-felipe-del-morro-castle.jpg?w=650&webp';
+import ImageSrcset from '$lib/assets/san-felipe-del-morro-castle.jpg?w=300;600;900&webp&srcset';
 
 // viz components
 import BarChart from '$lib/components/charts/barchart/index.svelte';
 import AI2HTML from '$lib/components/media/ai2html/ai2html.svelte';
-import { Image } from 'svelte-lazy-loader'
 
+// an array of our components, mapped to match the <figure data-chart=""> elements in index.html
 const components = [
 	{
-		chartID: 'fieri-img',
+		chartID: 'castle-img',
 		Component: Image,
 		props: {
 			loading: 'lazy',
-			alt: 'A picture of Flavortown Mayor Guy Fieri',
-			src: Fieri,
-			srcset: FieriSrcset,
+			alt: 'A few tourists walk up the lawn to the side of the old stone San Felipe del Morro Castle in San Juan, Puerto Rico',
+			placeholder: ImagePlaceholder,
+			src: ImageSrc,
+			srcset: ImageSrcset,
 			width: '650px',
-			classes: 'mx-auto'
+			classes:
+				'cmsvelte-mx-auto cmsvelte-block cmsvelte-w-full cmsvelte-h-auto cmsvelte-max-w-[720px]'
 		}
 	},
 	{
@@ -30,6 +38,39 @@ const components = [
 		Component: AI2HTML
 	}
 ];
+
+// add objects in the cmsFiles array to mimic styles and scripts in your CMS while developing. These scripts aren't included in production mode.
+const cmsFiles = [
+	// {
+	// 	tag: 'link',
+	// 	attributes: {
+	// 		href: 'https://www.site.com/path/to.css',
+	// 		rel: 'stylesheet',
+	// 		type: 'text/css'
+	// 	}
+	// },
+	// {
+	// 	tag: 'script',
+	// 	attributes: {
+	// 		src: 'https://www.site.com/path/to.js',
+	// 		async: 'true',
+	// 		type: 'text/javascript'
+	// 	}
+	// }
+];
+
+// add files in the format below to
+if (import.meta.env.DEV && cmsFiles.length > 0) {
+	const head = document.head;
+
+	cmsFiles.forEach(({ tag, attributes }) => {
+		const el = document.createElement(tag);
+
+		Object.entries(attributes).forEach(([key, value]) => (el[key] = value));
+
+		head.appendChild(el);
+	});
+}
 
 function mountComponent({
 	Component,
